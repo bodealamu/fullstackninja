93bd3bca3fe11cfcafd3d58910ab29ffb190efd5
olabodealamu <bodealamu007@gmail.com>
2020-06-13 17:03:01 -0500

    added custom user

--- accounts/admin.py
+++ accounts/admin.py
@@ -12,6 +12,18 @@ class UserAdmin(BaseUserAdmin):
     add_form = UserCreationForm
     list_display = ("email", "first_name", "last_name", "is_superuser", "is_staff")
     ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email","password")}),
+        ("Personal info", {"fields":("first_name", "last_name")}),
+        ("Permissions", {"fields":("is_superuser", "is_active", "is_staff")})
+    )
+    add_fieldsets = (
+        (None, {
+            "classes":('wide',),
+            "fields":('email', 'first_name','last_name','password1', 'password2')
+        }
+         ),
+    )
 
 
 admin.site.register(CustomUser, UserAdmin)
