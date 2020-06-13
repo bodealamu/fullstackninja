8e09ef6f2e3e2e0a0078af2a5ae58fba6ccd1356
olabodealamu <bodealamu007@gmail.com>
2020-06-12 23:30:59 -0500

    added custom user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from accounts.forms import UserChangeForm, UserCreationForm
+
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
