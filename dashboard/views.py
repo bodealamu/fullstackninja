29aefc65d9b113b68e8543ab04a612a5f4e3b4fd
olabodealamu <bodealamu007@gmail.com>
2020-06-15 19:32:15 -0500

    added views for admin page

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,3 +5,23 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request=request, context=None, template_name="dashboard/dashboard.html")
+
+
+def dashboard_category(request):
+    return render(request=request, context=None, template_name="dashboard/dashboard_category.html")
+
+
+def dashboard_staff(request):
+    return render(request=request, context=None, template_name="dashboard/dashboard_staff.html")
+
+
+def dashboard_subcategory(request):
+    return render(request=request, context=None, template_name="dashboard/dashboard_subcategory.html")
+
+
+def dashboard_tutorialseries(request):
+    return render(request=request, context=None, template_name="dashboard/dashboard_Tutorialseries.html")
+
+
+def dashboard_tutorialvideos(request):
+    return render(request=request, context=None, template_name="dashboard/dashboard_Tutorialvideos.html")
