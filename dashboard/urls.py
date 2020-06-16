a9d9d199d206591bda3bc8e74ae1ceaa72ceb277
olabodealamu <bodealamu007@gmail.com>
2020-06-15 19:25:12 -0500

    admin page setup

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from dashboard import views
 
 urlpatterns = [
+    path("", views.dashboard, name="dashboard"),
 
 
 ]
