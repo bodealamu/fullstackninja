15025e4320d1e5fe7612bdb3209a43324943fb4d
olabodealamu <bodealamu007@gmail.com>
2020-06-26 17:23:31 -0500

    updated aesthetics of forms.py in admin dashboard

--- catalogue/forms.py
+++ catalogue/forms.py
@@ -6,25 +6,43 @@ class TutorialForm(forms.ModelForm):
     class Meta:
         model = Tutorial
         fields = ["title","description","tutorial_category","author","tutorial_logo","github_repo_link"]
+        widgets = {
+            "title":forms.TextInput(attrs={'class':'form-control'}),
+            "description": forms.TextInput(attrs={'class': 'form-control'}),
+            "github_repo_link": forms.TextInput(attrs={'class': 'form-control'}),
+
+        }
 
 
 class TutorialVideoForm(forms.ModelForm):
     class Meta:
         model = TutorialVideo
         fields = ["video_title", "description", "tutorial_category", "author", "youtube_link"]
+        widgets = {
+            "video_title":forms.TextInput(attrs={'class':'form-control'}),
+            "description":forms.Textarea(attrs={'class':'form-control'}),
+            "youtube_link":forms.TextInput(attrs={'class':'form-control'}),
+        }
 
 
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ["title","author","description","category_image"]
+        widgets = {
+            "title":forms.TextInput(attrs={'class':'form-control'}),
+            "description":forms.Textarea(attrs={'class':'form-control'}),
+        }
 
 
 class SubCategoryForm(forms.ModelForm):
     class Meta:
         model = SubCategory
         fields = ["title","author","description","classification", "subcategory_logo"]
-
+        widgets = {
+            "title":forms.TextInput(attrs={'class':'form-control'}),
+            "description":forms.Textarea(attrs={'class':'form-control'}),
+        }
 
 
 
