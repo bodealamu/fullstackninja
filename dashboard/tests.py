2bf0ccaf73266a1198643a77a5dc279c250332a3
olabodealamu <bodealamu007@gmail.com>
2020-06-30 15:58:00 -0500

    added tests for dashboard urls

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 
 # Create your tests here.
+
+
+class DashboardTests(TestCase):
+    def test_dashboard_page_status_code(self):
+        response = self.client.get('/dashboard/')
+        self.assertEqual(first=response.status_code, second=200)
+
+    def test_dashboard_category_status_code(self):
+        response = self.client.get('/dashboard/category')
+        self.assertEqual(first=response.status_code, second=200)
