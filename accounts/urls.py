197e489ad0456548a5852df99aaa15c75e9db180
olabodealamu <bodealamu007@gmail.com>
2020-06-30 18:18:16 -0500

    password change form implemented

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,10 @@ from django.contrib.auth import views as authentication_views
 
 urlpatterns = [
     path("login/", authentication_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
-    path('logout', authentication_views.LogoutView.as_view(template_name="accounts/logout.html"), name='logout'),
+    path('logout/', authentication_views.LogoutView.as_view(template_name="accounts/logout.html"), name='logout'),
+    path("password_change/",
+         authentication_views.PasswordChangeView.as_view(template_name="accounts/password_change.html"),
+         name="password_change"),
 
 
 ]
