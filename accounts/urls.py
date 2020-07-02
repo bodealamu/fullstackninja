e312919659c8d945751f91cc30c8ae60cb0c8e5a
olabodealamu <bodealamu007@gmail.com>
2020-07-02 15:50:29 -0500

    Authentification views completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -18,8 +18,9 @@ urlpatterns = [
     path('password_reset/done/',
          authentication_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"),
          name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', authentication_views.PasswordResetConfirmView.as_view(template_name="accounts/passwordresetconfirm.html"), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', authentication_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/',
          authentication_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"),
          name='password_reset_complete'),
 ]
+# template_name="accounts/passwordresetconfirm.html"
