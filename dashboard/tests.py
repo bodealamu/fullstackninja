d6fe1d5e9aa3878b79bda0bf14b1d941f14ece03
olabodealamu <bodealamu007@gmail.com>
2020-07-06 17:27:16 -0500

    adding tests to the dashboard app

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -15,3 +15,23 @@ class DashboardTests(TestCase):
     def test_dashboard_staff_status_code(self):
         response = self.client.get('/dashboard/staff')
         self.assertEqual(first=response.status_code, second=200)
+
+    def test_dashboard_subategory_status_code(self):
+        response = self.client.get('dashboard/subcategory')
+        self.assertEqual(first=response.status_code, second=200)
+
+    def test_dashboard_tutorialseries_status_code(self):
+        response = self.client.get('dashboard/tutorialseries')
+        self.assertEqual(first=response.status_code, second=200)
+
+    def test_dashboard_tutorialvideos_status_code(self):
+        response = self.client.get('dashboard/tutorialvideos')
+        self.assertEqual(first=response.status_code, second=200)
+
+    def test_dashboard_profile_status_code(self):
+        response = self.client.get('dashboard/profile')
+        self.assertEqual(first=response.status_code, second=200)
+
+    def test_dashboard_messages_status_code(self):
+        response = self.client.get('dashboard/messages')
+        self.assertEqual(first=response.status_code, second=200)
