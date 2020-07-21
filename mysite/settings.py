8081c5987b4344c609032798095560d0cc24657e
olabodealamu <bodealamu007@gmail.com>
2020-07-21 14:05:21 -0500

    added awsebi to requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["fullstackninja.eba-psyskq48.us-east-1.elasticbeanstalk.com", '127.0.0.1']
 
 
 # Application definition
